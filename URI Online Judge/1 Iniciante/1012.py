ef596afdf397a5202dd4252bd3ecbd992dfd95e4
Nelson Antevere <nelson.antevere@sp.mz-ir>
2017-12-06 23:53:32 -0200

    Exercício 1012

--- URI Online Judge/1 Iniciante/1012.py
+++ URI Online Judge/1 Iniciante/1012.py
@@ -1,4 +1,10 @@
 x = input()
-y = input()
+a,b,c = x.split(' ')
+a,b,c = float(a),float(b),float(c)
+tri = (a*c) / 2
+cir = 3.14159 * (c**2)
+tra = ((a + b) * c) / 2
+qua = b*b
+ret = a*b
 
-a,b,c = float(x.split(' '))
+print('TRIANGULO: %.3f\nCIRCULO: %.3f\nTRAPEZIO: %.3f\nQUADRADO: %.3f\nRETANGULO: %.3f' %(tri, cir, tra, qua, ret))
